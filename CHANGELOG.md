b72fc646cdb3feb1ed41989a394a0cc2d27555ef
gabriel <gedera@wispro.co>
2026-01-26 18:14:03 -0300

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## [Unreleased]
+
+## [0.1.0] - 2026-01-26
+
+- Initial release
---
+++ lib/active_partition.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "active_partition/version"
+
+module ActivePartition
+  class Error < StandardError; end
+  # Your code goes here...
+end
